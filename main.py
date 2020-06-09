09570d27f8d2fc3d26895df5cb3a123287fbd6fc
crazydev901 <david90192@outlook.com>
2020-06-09 09:27:31 -0400

    feat(add function): message to new user function added

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import fastapi
+# from fastapi import FastAPI
+import os
+from slack import RTMClient
+from slack.errors import SlackApiError
 
-app = FastAPI()
+BOT_NAME = 'myslackbot'
+SLACK_API_TOKEN = "RMm19mCPsKfNiEMwsjirkXhN"
 
-@app.get("/")
-def home():
-    return {"Hello", "World"}
+@RTMClient.run_on(event='team_join')
+def message_to_new_user(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    user = data['user']
+    print("================")
+    print(user)
+    print("================")
+    try:
+        response = webclient.chat_postMessage(
+            text=f"Useful Resources",
+            as_user=true
+        )
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"] # str like 'invalid_auth', 'channel_not_found'
+        print(f"Got an error: {e.response['error']}")
+rtm_client = RTMClient(token=SLACK_API_TOKEN)
+rtm_client.start()
+
+
+# app = FastAPI()
+
+# @app.get("/")
+# def home():
+#     return {"Hello", "World"}
